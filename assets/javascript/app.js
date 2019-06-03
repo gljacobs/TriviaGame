257b7a6ac1a9f825ef2809e29be23acb405ea083
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-03 14:44:54 -0700

    add correct answer functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,14 @@
+var questions = ["q1", "q2", "q3", "q4", "q5"];
+var score = 0;
+count = 30;
+var intervalId = setInterval(stopwatch.count, 1000);
 
+$(submit).on("click", function(){
+    for(var i = 0; i < questions.length; i++) {
+        if($("input[name=" + questions[i] + "]:checked").val() === "correct") {
+            score++;
+        }
+    }
+    console.log(score);
+    score = 0;
+})
