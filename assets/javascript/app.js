d722eceeef03de619ad4b8dabf2011fba2484ad9
Gabriel Jacobs <gabriel.luke.jacobs@gmail.com>
2019-06-03 15:06:36 -0700

    implement timer and reset functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,23 @@
 var questions = ["q1", "q2", "q3", "q4", "q5"];
 var score = 0;
-count = 30;
-var intervalId = setInterval(stopwatch.count, 1000);
+var time = 30;
+
+var countDown = function(){
+    if(time > 0){
+        time--;
+        $("#time").text("Time remaining... " + time + " seconds");
+    }
+    else {
+        time = 30;
+        $("#time").text("Time remaining... " + time + " seconds");
+        score = 0;
+        for(var i = 0; i < questions.length; i++) {
+            $("input[name=" + questions[i] + "]:checked").prop("checked", false);
+        }
+    }
+}
+var intervalId = setInterval(countDown, 1000);
+
 
 $(submit).on("click", function(){
     for(var i = 0; i < questions.length; i++) {
@@ -9,6 +25,11 @@ $(submit).on("click", function(){
             score++;
         }
     }
+    time = 30;
+    $("#time").text("Time remaining... " + time + " seconds");
     console.log(score);
     score = 0;
+    for(var i = 0; i < questions.length; i++) {
+        $("input[name=" + questions[i] + "]:checked").prop("checked", false);
+    }
 })
